77106903d5910676baaef8eb8b2a68369ff0b686
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-18 11:34:55 +0500

    Services added

---
+++ Mock_interview/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mock_interview.DTOs;
+using Mock_interview.Mock_interviewServiceTools;
+
+namespace Mock_interview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        
+         
+        
+    }
+}
