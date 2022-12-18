9fb23c8fed7be599f916f82b62f4eaa36efd79c6
Alimkhan <olimxonnutfullaevich@gmail.com>
2022-12-18 12:30:16 +0500

    Changed Services

--- Mock_interview/Entities/Interviewer.cs
+++ Mock_interview/Entities/Interviewer.cs
@@ -4,9 +4,9 @@
     {
         public int InterviewerId { get; set; }
         public int UserId { get; set; }
+        public User User { get; set; }
         public string Projects { get; set; }
         public int AbilityId { get; set; }
         public Ability Ability { get; set;}
-        public User User { get; set; }
     }
 }
