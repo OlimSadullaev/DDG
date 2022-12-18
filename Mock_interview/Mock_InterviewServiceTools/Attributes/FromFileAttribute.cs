f54c13e206d117e2a26d000a17b59c6c439357ea
Haydarovcoder <abboshaydarov0799@gmail.com>
2022-12-18 11:52:29 +0500

    Added Attributes, Exceptions and Helpers to MockInterviewServiceTools folder

---
+++ Mock_interview/Mock_InterviewServiceTools/Attributes/FromFileAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mock_interview.Mock_InterviewServiceTools.Attributes
+{
+    public class FromFileAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                string[] extensions = new string[] { ".png", ".jpg" };
+                var extension = Path.GetExtension(file.FileName);
+
+                if (!extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult("This photo extension is not allowed!");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
